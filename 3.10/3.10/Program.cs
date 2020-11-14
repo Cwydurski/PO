571c0ae3c6d793183cd2a2c844c7f4a17b842436
Cezary Wydurski <Cezary Wydurski@DESKTOP-VSKMBJ8>
2020-11-14 12:50:46 +0100

    zadania

---
+++ 3.10/3.10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _3._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i = 0, silnia = 1;
+                Console.WriteLine("Podaj n");
+           n=Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+                Console.WriteLine("n nie moze byc mniejsze od 0");
+            else
+            {
+                if (i >= n)
+                    Console.WriteLine(silnia);
+
+                else if (i < n)
+                {
+                    while (i < n)
+                    {
+                        i++;
+                        silnia *= i;
+
+                    }
+                    Console.WriteLine("silnia wynosi:");
+                    Console.WriteLine(silnia);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
