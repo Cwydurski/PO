9aa567d970c57b43bbe27527619cd08369d0f8e1
Cezary Wydurski <Cezary Wydurski@DESKTOP-VSKMBJ8>
2020-11-14 12:38:07 +0100

    zadania

---
+++ 3.1/3.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ROK;
+            Console.WriteLine("Podaj rok");
+            ROK = Convert.ToInt32(Console.ReadLine());
+            if ((ROK % 4 == 0 && ROK % 100 != 0) || (ROK % 400 == 0))
+                Console.WriteLine("Rok jest przestepny");
+            else
+                Console.WriteLine("Rok nie jest przestepny");
+            Console.ReadKey();
+
+        }
+    }
+}
