eb9d9887a86d5a9685af05c9dcece58f54766579
Cezary Wydurski <Cezary Wydurski@DESKTOP-VSKMBJ8>
2020-11-21 12:50:07 +0100

    Zadania tablice

---
+++ 4.1/4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] tablica = new string[n];
+            for (int i = 0; i < n ; i++)
+            {
+                Console.WriteLine("Podaj wartosc");
+                tablica[i] = Console.ReadLine();
+                
+            }
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                Console.Write(tablica[i] + ", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
