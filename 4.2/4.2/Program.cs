dafd27f65ab95aa816360a09b56779d6819c20db
Cezary Wydurski <Cezary Wydurski@DESKTOP-VSKMBJ8>
2020-11-21 14:08:53 +0100

    Zadania rozdział 4

---
+++ 4.2/4.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] tab2 = new int[10];
+            int[] tab1 = new int[10];
+            for (int i = 0; i < tab1.Length; i++)
+            {
+                Console.WriteLine("Podaj wartosc");
+                tab1[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            
+            for (int i = 0; i < tab1.Length; i++)
+            {
+                
+                if (tab1[i] > 0)
+                {
+                    tab2[i] = tab1[i];
+                    
+
+            }
+                Console.WriteLine(tab2[i] + ",");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
