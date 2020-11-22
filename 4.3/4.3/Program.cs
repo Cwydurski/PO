8c7d220d7df711ee9f6f0064bc01e118dad29207
Cezary Wydurski <Cezary Wydurski@DESKTOP-VSKMBJ8>
2020-11-22 12:19:26 +0100

    Zadania rozdział 4

---
+++ 4.3/4.3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz ilosc elementow tablicy");
+            int IloscIndexow = int.Parse(Console.ReadLine());
+            int[] tablica = new int[IloscIndexow];
+            int najmniejszy = 0;
+            int najwiekszy = 0;
+            double sredniaWszystkich = 0;
+            int liczbaDodatnichElementow = 0;
+
+
+            for (int x = 0; x < tablica.Length; x++)
+            {
+                Console.WriteLine("Wpisz wartosc do elementu tablicy oraz nacisnij 'enter'");
+                tablica[x] = int.Parse(Console.ReadLine());
+
+                if (najmniejszy > tablica[x])
+                {
+                    najmniejszy = tablica[x];
+                }
+                if (najwiekszy < tablica[x])
+                {
+                    najwiekszy = tablica[x];
+                }
+                if (tablica[x] > 0)
+                {
+                    liczbaDodatnichElementow++;
+                }
+                sredniaWszystkich += tablica[x];
+            }
+            int a = Array.IndexOf(tablica, najmniejszy);
+            int b = Array.IndexOf(tablica, najwiekszy);
+            sredniaWszystkich = sredniaWszystkich / tablica.Length;
+            Console.WriteLine("\nNajmniejszy element wynosi: {0} wartość tego elementu znajduje się w indexie: {1} \nNajwiekszy element wynosi: {2} wartość tego elementu znajduje się w indexie: {3} \nLiczba dodatnich elementow wynosi: {4} \nSrednia wartosci wszystkich elementow wynosi: {5}", najmniejszy, a, najwiekszy, b, liczbaDodatnichElementow, sredniaWszystkich);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4.4/4.4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = new int[100];
+            Random randomoweNaturalne = new Random();
+            int kubelek = 2;
+
+            for (int x = 0; x < tablica.Length; x++)
+            {
+                tablica[x] = randomoweNaturalne.Next(2, 1000);
+                int liczba = tablica[x];
+
+                for (int y = kubelek; y <= liczba; y++)
+                {
+                    if (liczba % liczba == 0 && liczba % 1 == 0)
+                    {
+                        if (liczba == 2)
+                        {
+                            Console.WriteLine("{0} to liczba pierwsza", liczba);
+                            kubelek = 2;
+                            break;
+                        }
+                        if (liczba % kubelek != 0)
+                        {
+                            if (liczba == kubelek + 1)
+                            {
+                                Console.WriteLine("{0} to liczba pierwsza", liczba);
+                                kubelek = 2;
+                                break;
+                            }
+                            else
+                            {
+                                kubelek++;
+                            }
+                        }
+                        else
+                        {
+                            
+                            kubelek = 2;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
